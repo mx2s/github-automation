3766dbff0fa090d9ee80cb4de42e2ed4f8c94cf7
Max Kovalenko <mxsshere@gmail.com>
2019-12-23 21:22:52 -0500

    basic script for creating new issues

--- new_issue.py
+++ new_issue.py
@@ -1,10 +1,45 @@
 import json
-from urllib.request import Request, urlopen
+from github import Github
 
 config = json.loads(open('./config/config.json', 'r').read())
 
-req = Request('https://api.github.com/repos/mxss/gitcom-api/issues/1')
-req.add_header('Authorization', 'xxx')
-content = urlopen(req).read()
+github_api = Github(login_or_token=config['auth']['github']['token'])
 
-print(content)
+repo = github_api.get_repo('mxss/gitcom-api')
+
+default_labels = [
+    repo.get_label("help wanted"),
+    repo.get_label("good first issue"),
+]
+
+issue_type_labels = [
+    repo.get_label("enhancement"),
+    repo.get_label("bug"),
+    repo.get_label("documentation"),
+]
+
+while True:
+    try:
+        issue_title = str(input('Type issue title:'))
+
+        issue_labels = default_labels.copy()
+
+        print('Select issue type:')
+        issue_type_index = 1
+        for type_label in issue_type_labels:
+            print('{0} - {1}'.format(issue_type_index, type_label.name))
+            issue_type_index += 1
+        selected_type_index = int(input('Enter number:')) - 1
+
+        issue_labels.append(issue_type_labels[selected_type_index])
+
+        print(issue_labels)
+
+        created_issue = repo.create_issue(title=issue_title, labels=issue_labels)
+
+        print(created_issue)
+        print('Issue #{0} was created!'.format(created_issue.number))
+        print('***\n')
+    except Exception as e:
+        print(e)
+        print('Error!\n\n')
